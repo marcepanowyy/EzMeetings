cdab550bdc6d54babcf7557bd8f6ec7d0bfeb4d8
Adam Biśta <410788@student.agh.edu.pl>
2023-12-10 00:58:10 +0100

    fix: remove tanstack-query

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -11,8 +11,6 @@ import EditEvent from "./components/event/EditEvent";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
-import { queryClient } from "./utils/http.js";
-import { QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
   const router = createBrowserRouter([
@@ -62,9 +60,7 @@ function App() {
   ]);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />;
-    </QueryClientProvider>
+      <RouterProvider router={router} />
   );
 }
 
